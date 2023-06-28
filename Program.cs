022e37f52b53c10e5f4273ddd7ea8dd6f0475352
DanAndy <113089418+DanAndy@users.noreply.github.com>
2023-06-28 11:13:26 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace popovzd24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int sum = 0;
+            int i = 0;
+
+            while (i < 3)
+            {
+                Random rnd = new Random();
+                int a = rnd.Next(1, 24);
+                int b = rnd.Next(1, 24);
+                int c = rnd.Next(1, 24);
+                Console.WriteLine(a);
+                Console.WriteLine(b);
+                Console.WriteLine(c);
+                int x = Convert.ToInt32(Console.ReadLine());
+                if (x == a || x == b || x == c)
+                {
+                    sum++;
+                }
+                i++;
+            }
+
+            if(sum >= 2)
+                Console.WriteLine("Вы победили!");
+            else if (sum < 2)
+                Console.WriteLine("Вы проиграли!");
+        }
+    }
+}
